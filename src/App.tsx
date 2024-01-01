e6465c790b8784167b6ba628a619e37341f1f162
tuanpc1902 <82777525+tuanpc1902@users.noreply.github.com>
2024-01-02 02:26:16 +0700

    add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { useTranslation } from "react-i18next";
 import i18n from "./i18n/i18n";
 import { Select, Space } from "antd";
+import AccountWithdrawalPage from "./pages/account/account-withdrawal/account-withdrawal.page";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     const translate = useTranslation("general").t;
     const language = i18n.language;
 
@@ -18,11 +14,12 @@ function App() {
 
     return (
         <>
+
             <Space size={20} align="center" direction="vertical" wrap>
-                <Select
+            <Select
                     defaultValue={language ?? "English"}
                     onChange={onChangeLanguage}
-                    style={{ width: 120 }}
+                    style={{ width: 120, position: 'absolute', top: 50 }}
                     options={[
                         {
                             value: "en",
@@ -34,29 +31,10 @@ function App() {
                         },
                     ]}
                 />
-                <div className="language">{`Language: ${language}`}</div>
+                <div className="language">{`Current language: ${language}`}</div>
                 <div className="test-translate">{translate("test")}</div>
+                <AccountWithdrawalPage />
             </Space>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
---
+++ src/pages/account/account-withdrawal/account-withdrawal.page.tsx
@@ -0,0 +1,81 @@
+import { Button, Checkbox, Form, Input } from "antd";
+import { useTranslation } from "react-i18next";
+
+type FieldType = {
+    username?: string;
+    password?: string;
+    remember?: string;
+};
+
+type ErrorField = {
+    errors: string[];
+    name: string[];
+    warning: string[];
+};
+
+type FinishFailed = {
+    errorFields: ErrorField[];
+    outOfDate: string;
+    values: FieldType;
+};
+
+function AccountWithdrawalPage() {
+    const translate = useTranslation("account-withdrawal-page").t;
+    const onFinish = (values: FieldType) => {
+        console.log("Success:", values);
+    };
+
+    const onFinishFailed = (errorInfo: FinishFailed) => {
+        console.log("Failed:", errorInfo);
+    };
+
+    return (
+        <div className="account-withdrawal-page">
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item<FieldType>
+                    label={translate("username")}
+                    name="username"
+                    rules={[
+                        { required: true, message: translate("pleaseInputYourUsername") },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                    label={translate("password")}
+                    name="password"
+                    rules={[
+                        { required: true, message: translate("pleaseInputYourPassword") },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                    name="remember"
+                    valuePropName="checked"
+                    wrapperCol={{ offset: 8, span: 16 }}
+                >
+                    <Checkbox>{translate("rememberMe")}</Checkbox>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        {translate("submit")}
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+}
+export default AccountWithdrawalPage;
