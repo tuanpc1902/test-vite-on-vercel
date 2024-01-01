1f2949d914001c688272a5b82805c93f8b42cc53
tuanpc1902 <82777525+tuanpc1902@users.noreply.github.com>
2024-01-02 03:14:39 +0700

    build language

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { copyLocalesPlugin } from './vite-plugin-copy-locales';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
-        plugins: [react()],
-    }
-})
+        plugins: [
+            react(),
+            copyLocalesPlugin()
+        ],
+    };
+});
